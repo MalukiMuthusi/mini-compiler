a0defcc2c308101daef95b73f6fb82fc51a37353
Maluki Muthusi Maluki P15/81741/2017 <malukimuthusi@students.uonbi.ac.ke>
2021-03-29 13:53:52 +0300

    Feat: Parser

---
+++ assignment_VIII/lexer/Real.java
@@ -0,0 +1,16 @@
+package lexer;
+
+public class Real extends Token {
+
+    public final float value;
+
+    public Real(float v) {
+        super(Tag.REAL);
+        value = v;
+    }
+
+    @Override
+    public String toString() {
+        return "" + value;
+    }
+}
